bad78e413cc04660a0444af99a1e1f684d64eeee
Fowotade Babajide <jide.b.tade@gmail.com>
2020-11-13 16:22:44 +0100

    chore: build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,6 @@
 import { Application } from './types';
 declare function Application(): Application;
-declare const _default: typeof Application;
 /**
  * Expose `Application()`.
  */
-export = _default;
+export default Application;
