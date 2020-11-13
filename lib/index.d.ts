f7b08dad758f719e1e858b7437b89ca8fbd84a98
Fowotade Babajide <jide.b.tade@gmail.com>
2020-11-13 11:46:03 +0100

    chore: remove lib from gitignore for easy module access for use

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+import { Application } from './types';
+declare function Application(): Application;
+declare const _default: typeof Application;
+/**
+ * Expose `Application()`.
+ */
+export = _default;
